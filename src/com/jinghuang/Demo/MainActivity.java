e0971821dd2383811e62864d0792ff719b570a4c
hakimhuang <hakimhuang@tencent.com>
2016-01-08 16:07:27 +0800

    add new android project

---
+++ src/com/jinghuang/Demo/MainActivity.java
@@ -0,0 +1,15 @@
+package com.jinghuang.Demo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by hakimhuang on 2016/1/8.
+ */
+public class MainActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
